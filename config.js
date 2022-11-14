a7a93d7c2acb6286955566faf4205314edc9ebce
king1wa <111672626+king1wa@users.noreply.github.com>
2022-11-14 18:36:06 +0500

    Update config.js

--- config.js
+++ config.js
@@ -67,10 +67,10 @@ global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
 global.psaweria = 'https://saweria.com/mrw4h1d'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62XXXXXXXXXXX'
-global.nomorown = '6282127487538'
-global.namebot = 'WH-MODS-BOT-V1'
-global.nameown = 'WH MODS DEV'
+global.nomorbot = '923266641991'
+global.nomorown = '923266641991'
+global.namebot = '𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇1'
+global.nameown = '𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇'
 
 /*============== Github Thanks To ==============*/
 global.namagithub = 'Nama Github Belum Diisi' // nama github lu
@@ -138,14 +138,14 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 WH-MODS-BOT-V1 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘WH-MODS-BOT-V1 ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ WH-MODS-BOT-V1 '
+global.wm = '                「 𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇 」' //Main Watermark
+global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
+global.wm3 = '⫹⫺ 𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇'
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `⫹⫺ WhatsApp Bot | By WH-MODS-DEV`
+global.titlebot = `⫹⫺ WhatsApp Bot |𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇
 global.packname = '☂︎ ᴄʀᴇᴀᴛᴇᴅ ʙʏ'
-global.author = '                「 WH-MODS-BOT-V1  あ」'
+global.author = '                「 𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇 あ」'
 
 /*Fake*/
 global.fsizedoc = '1'.repeat(10)
@@ -636,8 +636,8 @@ global.htjava = '⫹⫺'    //hiasan Doang :v
 global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
-global.stickpack = 'WH-MODS-BOT-V1'
-global.stickauth = `By ©WH-MODS-DEV`
+global.stickpack = '𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇1'
+global.stickauth = `By ©𝐒𝐀𝐍𝐖𝐀𝐋 𝐁𝐀𝐋𝐎𝐂𝐇
 
 global.multiplier = 38 // The higher, The harder levelup
 
